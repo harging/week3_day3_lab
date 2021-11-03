36475252b586d150861832778da897758cc61b6c
Gavin Hargin <gavin.hargin@gmail.com>
2021-11-03 14:57:51 +0000

    added orders

---
+++ controllers/controller.py
@@ -0,0 +1,3 @@
+from flask import render_template
+from shop import shop
+from models.orders import orders
---
+++ models/orders.py
@@ -0,0 +1,5 @@
+from models.order import *
+
+order1 = Order("Andrew", "03-11-2021", 10)
+order2 = Order("Gavin", "02-11-2021", 20)
+orders = [order1, order2]
